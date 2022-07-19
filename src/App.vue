9edcc67b635ebbe6701d4d12177e0a38fa3f4e94
eduleap.2022n.38 <eduleap.2022n.38@gmail.com>
2022-07-19 18:41:43 +0900

    API取得

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,38 @@
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  aaa
+  <section class="alert alert-primary">
+    <textarea v-model="data.mydata"></textarea>
+  </section>
 
 </template>
 
-
-
-
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-// import  Chart  from ""
+//js
+import axios from 'axios';
+import { reactive, onMounted } from 'vue';
+
+const url = "https://opendata.resas-portal.go.jp/api/v1/prefectures"
+const headers = {
+  'X-Api-Key':'y3y97v7GaGN3GQLKCKxaaAXeKNxmLVZb76UWyV1O'
+}
+const data = reactive({
+  mydata: "",
+});
+const getData = async () =>{
+  console.log('aaa')
+  let result = await axios.get(url, {headers});
+  console.log(result)
+  data.mydata = result.data;
+  };
+
+onMounted(() => {
+  getData();
+
+});
 </script>
 
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+/* CSS */
 </style>
