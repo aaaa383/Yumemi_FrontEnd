0c2a99df586ed7de85891ade0add1fc1a2ab8b9c
AIF-Akane <108518211+AIF-Akane@users.noreply.github.com>
2022-08-01 16:33:45 +0900

    Feature test2 (#4)
    * Vueプロジェクト立ち上げ
    
    * API取得
    
    * チェックボックスの作成・人口構成の取得
    
    * packege-jsonの修正
    
    * 0727
    
    * 0729
    
    * グラフ削除
    
    Co-authored-by: eduleap.2022n.38 <eduleap.2022n.38@gmail.com>
    Co-authored-by: Kenta Hoji <58076642+aaaa383@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,125 @@
 <template>
-  <div>
-    <li v-for="(item, index) in items">
+  <div id="mycheckbox">
+    <p v-for="(item, index) in items">
       <input
         type="checkbox"
-        id="checked"
-        v-model="check_box"
-        @input="onClick($event, item.prefCode)"
+        id="'checked' + index"
+        @input="onClick($event, item.prefCode, item.prefName)"
       />
-      <label for="checked"
-        >{{ index }}-{{ item.prefCode }}-{{ item.prefName }}</label
-      >
-    </li>
+      <label for="'checked' + index">{{ item.prefName }} </label>
+    </p>
   </div>
+  <!-- {{ chartdata }} -->
+  <LineChart ref="chart" :chart-data="chartdata" />
 </template>
 
 <script setup>
 //js
 import axios from "axios";
-import { ref, reactive, onMounted } from "vue";
-import { Chart } from "chart.js";
+import { ref, onMounted, defineComponent } from "vue";
+import { Chart, registerables } from "chart.js";
+import { LineChart } from "vue-chart-3";
+
 
 const url = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
-const url2 =
-  "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=";
+const url2 ="https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=";
 const headers = {
   "X-Api-Key": "y3y97v7GaGN3GQLKCKxaaAXeKNxmLVZb76UWyV1O",
 };
 
 const getData = async () => {
-  console.log("aaa");
   let { data } = await axios.get(url, { headers });
-  console.log(data);
+  // console.log(data);
   items.value = data.result;
 };
 
 const items = ref([]);
 
-const onClick = async (event, prefCode) => {
-  if (event.target.checked) {
+const onClick = async (event, prefCode, prefName) => {
+  if (event.target.checked == true) {
     // console.log(test)
     // let {data}  = await axios.get(url, {headers});
     const URL_population = url2 + prefCode;
-    let data2 = await axios.get(URL_population, { headers });
-    console.log(data2);
-    console.log();
+    await axios.get(URL_population, { headers }).then((res) => {
+      const pref = res.data.result.data[0].data; //総人口のデータのみ取得
+      const props = {
+        labels: {
+          type: Array,
+          requires: true,
+        },
+        datasets: {
+          type: Object,
+          requires: true,
+        },
+      };
+
+      const { labels } = ref(props);
+
+      const year = [];
+      const value = [];
+      const namelabel = prefName;
+
+      //それぞれに格納している
+      pref.forEach((element) => {
+        year.push(element.year);
+      });
+
+      pref.forEach((element) => {
+        if (value.includes(element.value) == false) {
+          value.push(element.value);
+        }
+      });
+
+      const pref_data = {
+        label: namelabel,
+        data: value,
+      };
+
+      all_data.value.push(pref_data);
+
+      const data3 = {
+        labels: year,
+        datasets: all_data.value,
+      };
+      chartdata.value = data3;
+    });
+  } else {
+    const index = all_data.value.findIndex((el) => el.label === prefName);
+    all_data.value.splice(index, 1);
+    console.log(chart.value)
+    chart.value.chartInstance.data.datasets.splice(index, 1);
   }
 };
-//クリックしたときにそれぞれの都道府県のPrefCodeを取る。
 
-var myLinChart = new Chart()
-
+const all_data = ref([]);
+const year = ref([]);
+const chart = ref();
+console.log(chart.value)
 
 onMounted(async () => {
   await getData();
-  // onClick();
+  console.log(chart.value)
 });
+
+const name = defineComponent("myChart");
+const components = {
+  Chart,
+};
+const chartdata = ref({});
+const direction = ref("horizontal");
+</script>
+
+<script>
+Chart.register(...registerables);
 </script>
 
 <style scoped>
-/* CSS */
+#mycheckbox {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#mycheckbox div {
+  width: 25%;
+}
 </style>
